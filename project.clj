e64c05925f5ee6a098f354fe1434333ba9babdde
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-07-12 22:19:46 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,64 @@
+(defproject reftest "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.6.1"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [reagent "0.6.0-rc"]]
+
+  :plugins [[lein-figwheel "0.5.4-7"]
+            [lein-cljfmt "0.5.3"]
+            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                ;; the presence of a :figwheel configuration here
+                ;; will cause figwheel to inject the figwheel client
+                ;; into your build
+                :figwheel {:on-jsload "reftest.core/on-js-reload"
+                           ;; :open-urls will pop open your application
+                           ;; in the default browser once Figwheel has
+                           ;; started and complied your application.
+                           ;; Comment this out once it no longer serves you.
+                           :open-urls ["http://localhost:3449/index.html"]}
+
+                :compiler {:main reftest.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/reftest.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true}}
+               ;; This next build is an compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/reftest.js"
+                           :main reftest.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+
+             :css-dirs ["resources/public/css"]}
+
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.4-7"]
+                                  [com.cemerick/piggieback "0.2.1"]]
+                   ;; need to add dev source path here to get user.clj loaded
+                   :source-paths ["src" "dev"]
+                   ;; for CIDER
+                   ;; :plugins [[cider/cider-nrepl "0.12.0"]]
+                   :repl-options {; for nREPL dev you really need to limit output
+                                  :init (set! *print-length* 50)
+                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
---
+++ src/reftest/core.cljs
@@ -0,0 +1,28 @@
+(ns reftest.core
+  (:require [reagent.core :as reagent]
+            [goog.object :as gobj]))
+
+(enable-console-print!)
+
+(println "This text is printed from src/reftest/core.cljs. Go ahead and edit it and see reloading in action.")
+
+(defonce app-state (reagent/atom {:text "Hello world!"}))
+
+(defn hello-component
+  []
+  (let [dom-node (atom nil)]
+    (reagent/create-class
+     {:component-did-mount (fn [x] (js/console.log @dom-node))
+      :component-will-unmount (fn [] (reset! dom-node nil))
+      :reagent-render (fn [x] [:h2 {:ref (fn [x] (reset! dom-node x))} "hello"])})))
+
+(defn hello-world
+  []
+  [:div
+   [:h1 (:text @app-state)]
+   [hello-component]])
+
+(reagent/render-component [hello-world]
+                          (. js/document (getElementById "app")))
+
+(defn on-js-reload [])
